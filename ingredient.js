942c9666823b7471dab7d0c34ee17c068ea37bfe
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-03-19 15:02:13 -0400

    Added tables to recipe areas

--- ingredient.js
+++ ingredient.js
@@ -3,75 +3,68 @@ var blenderEl = document.getElementById("blender");
 var drinkRecipesEl = document.getElementById("drink-recipes");
 var groceriesEl = document.getElementById("groceries");
 var ingredientsEl = document.getElementById("ingredients");
-var inputEl =  document.getElementById("food-input");
+var inputEl = document.getElementById("food-input");
 
-$("#pantrySubmitBtn").click(function(){
-  addFood()
+$("#pantrySubmitBtn").click(function() {
+  addFood();
+  $("#ingredients").val("");
 });
 
 var foodItems = [];
 
-    function addFood() {
-  
-    var foodInput = $("#ingredients").val();
-    foodItems.push(foodInput);
-    document.getElementById("pantry").innerHTML = foodItems};
-  
+function addFood() {
+  var foodInput = $("#ingredients").val();
+  foodItems.push(foodInput);
+  document.getElementById("pantry").innerHTML = foodItems;
+}
 
-   // should trigger only when generate button is clicked 
-  //  $("#").click(function(){
-     
+// should trigger only when generate button is clicked
+//  $("#").click(function(){
 
-  var foodIngredients = foodItems
-  var queryFoodURL = "https://api.edamam.com/search?q=" + foodIngredients + "&app_id=4706df62&app_key=85b9a750ec733a4ab16ba62ec5d80e59"
+var foodIngredients = foodItems;
+var queryFoodURL =
+  "https://api.edamam.com/search?q=" +
+  foodIngredients +
+  "&app_id=4706df62&app_key=85b9a750ec733a4ab16ba62ec5d80e59";
 
-  $.ajax({
-    url: queryFoodURL,
-    method: "GET"
-  })
-  
-  .then(function(response) {
-        console.log(response.hits)
+$.ajax({
+  url: queryFoodURL,
+  method: "GET"
+})
+.then(function(response) {
+  console.log(response.hits);
+});
 
-  });
-
-// });   this is the closing curley bracket for the generate food recipe function 
+// });   this is the closing curley bracket for the generate food recipe function
 
 var barItems = [];
 
-console.log(barItems)
+console.log(barItems);
 
-$("#blenderSubmitBtn").click(function(){
-  addBar()
+$("#blenderSubmitBtn").click(function() {
+  addBar();
 });
 
 function addBar() {
+  var barInput = $("#ingredients").val();
+  barItems.push(barInput);
+  document.getElementById("blender").innerHTML = barItems;
+}
 
-
-var barInput = $("#ingredients").val();
-barItems.push(barInput);
-document.getElementById("blender").innerHTML = barItems};
-
-
-// should trigger only when generate button is clicked 
+// should trigger only when generate button is clicked
 
 // $("#").click(function(){
-  
 
-var drinkIngredients = barItems
-var queryDrinkURL = "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" + drinkIngredients;
- 
-  $.ajax({
-    url: queryDrinkURL,
-    method: "GET"
-    
-  }) 
-  .then(function(response) {
-        console.log(response)
+var drinkIngredients = barItems;
+var queryDrinkURL =
+  "https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=" +
+  drinkIngredients;
 
-  });
+$.ajax({
+  url: queryDrinkURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
 
-// });  this is the closing curley bracket for the generate drink recipes   
-
-  
- 
+// });  this is the closing curley bracket for the generate drink recipes
