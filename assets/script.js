01f02469015456ae9f3a3d9af05d5fd392ca55b7
John Kang <pollyfactor@hotmail.com>
2020-03-19 13:16:46 -0400

    added eventlisteners for our buttons

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    $("#pantrySubmitBtn").on("click", (function() {
+        console.log("Pantry");
+    }));
+
+    $("#blenderSubmitBtn").on("click", (function() {
+        console.log("Blender");
+    }))       
+
+});
